9ae357e0d267864e7a5b37805ae760ec9de9aa50
Abdul wahed <mohammadwahed49@gmail.com>
2025-10-17 18:21:49 +0600

    python

---
+++ loop.py
@@ -0,0 +1,13 @@
+# for i in range(100):
+#     print("I want to be a great programmer")
+
+result =0 
+# for i in range(50):
+#    result= result+1
+
+
+# range(start,n)
+for _ in range(2,50):
+    result= result+1
+    
+print(result)
--- turtle1.py
+++ turtle1.py
@@ -3,11 +3,15 @@ import turtle
 turtle.shape("turtle")
 turtle.speed(1)
 
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+
+
+
 turtle.exitonclick()
