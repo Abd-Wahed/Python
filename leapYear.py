c49131d24a19e1c7c363876a0abf34e9fa94927e
Abdul wahed <mohammadwahed49@gmail.com>
2025-10-17 16:46:35 +0600

    pyhton practice

---
+++ leapYear.py
@@ -0,0 +1,14 @@
+year = input("Enter a year: ")
+year = int(year)
+
+if year % 4 ==0:
+    if year % 100 ==0:
+        if year %400 ==0:
+            print("Yes")
+        else:
+            print("No")
+    else:
+        print("Yes")
+else:
+    print("No")
+        
---
+++ leapYear1.py
@@ -0,0 +1,8 @@
+year = input("Enter a year: ")
+year = int(year)
+if year % 100 !=0 and year %4 ==0:
+    print("Yes")
+elif year %100 ==0 and year %4 ==0:
+    print("Yes")
+else:
+    print("No")
