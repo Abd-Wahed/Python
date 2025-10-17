8e3dcbb267a322d92a40eaacf69d1a8c74984a15
Abdul wahed <mohammadwahed49@gmail.com>
2025-10-17 16:04:20 +0600

    First commit

---
+++ practice1.py
@@ -0,0 +1,10 @@
+# if condition
+saarc=["Bangladesh","Afgabistan","Bhutan","Nepal","India","pakistan","sri-lanka"]
+
+country = input("Enter the name of the country : ")
+if country in saarc :
+    print(country, "is a member of saarc")
+else:
+    print(country, "is not member of saarc")
+    
+print("program terminate") 
