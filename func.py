2dacbb9f41a26cbd85c8881965abef1b86320954
Abdul wahed <mohammadwahed49@gmail.com>
2025-10-18 16:39:07 +0600

    python

---
+++ func.py
@@ -0,0 +1,14 @@
+# def add(n1,n2):
+#     return n1+n2
+# n=10
+# m=10
+# result = add (n,m)
+# print(result)
+
+def myfnc(x,y=10,z=10):
+    print("x= ",x,"y= ",y,"z= ",z)
+    
+x=5
+y=6
+z=7
+myfnc(x,y,z)
